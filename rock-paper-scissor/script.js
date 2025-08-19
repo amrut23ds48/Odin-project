80789f9a9ec9177f15299e1c43c30692ce29ae64
amrut <noamrut@gmail.com>
2025-08-19 23:58:47 +0530

    write script for game

---
+++ rock-paper-scissor/script.js
@@ -0,0 +1,61 @@
+let humanScore = 0;
+let computerScore = 0;
+function getComputerChoice(){
+    let number = Math.floor(Math.random() * 3);
+    if(number === 0){
+        return "rock";
+    } else if(number === 1){
+        return "paper";
+    } else {
+        return "scissor";
+    }   
+}
+function getHumanChoice(){
+    choice = prompt("Make your choice: ");
+    return choice.toLowerCase();
+}
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === "rock" && computerChoice ==="paper"){
+        computerScore++;
+        console.log("you lose! paper beats rock!");   
+          
+    }else if(humanChoice === "paper" && computerChoice ==="rock"){
+        humanScore++;
+        console.log("you win! paper beats rock!");
+        
+    }else if(humanChoice === "paper" && computerChoice ==="scissor"){
+        computerScore++;
+        console.log("you lose! scissor beats rock!");
+        
+    }else if(humanChoice === "scissor" && computerChoice ==="paper"){
+        humanScore++;
+        console.log("you win! scissor beats rock!");
+        
+    }else if(humanChoice === "scissor" && computerChoice ==="rock"){
+        computerScore++;
+        console.log("you lose! rock beats scissor!");
+        
+    }else if(humanChoice === "rock" && computerChoice ==="scissor"){
+        humanScore++;
+        console.log("you win! rock beats scissor!");
+        
+    }else{
+        console.log("Its a draw! nobody wins");
+    }
+}
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        let human = getHumanChoice();
+        let computer = getComputerChoice();
+        playRound(human, computer);
+    }
+}
+function declareWinner(humanScore, computerScore){
+    const result = (humanScore === computerScore) ? 
+                    "The game was draw! play again?":
+                    (humanScore > computerScore) ?
+                    "You are final winner!":
+                    "You are loser :)";
+    return result;
+}
